aee3a79decd8763e20c31feff13592aa2a6c2422
Bear <csaba.ferenclaszlo@zapier.com>
2021-09-02 12:47:30 +0200

    added test coverage check

---
+++ check_coverage.py
@@ -0,0 +1,20 @@
+import sys
+from bs4 import BeautifulSoup
+
+
+
+coverage_percent:int
+coverage_threshold = int(sys.argv[1])
+
+with open('htmlcov/index.html', 'r') as coverage_file: #pylint:disable=unspecified-encoding
+    soup = BeautifulSoup(coverage_file.read(), "html.parser")
+    coverage_percent =  int(soup.find('span',{'class':'pc_cov'}).text.replace('%',''))
+
+if coverage_percent < coverage_threshold:
+    print(
+        'Coverage of ' + str(coverage_percent) +' is below threshold of ' + str(coverage_threshold)
+        )
+    sys.exit(1)
+else:
+    print('Good enough coverage! (%s)', coverage_percent)
+    sys.exit(0)
